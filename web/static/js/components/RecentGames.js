bd4b8408f8888cff26e2a7330ed120da9821a979
Joseph <joe@josephyi.com>
2016-06-07 14:32:47 -0700

    recent games stuff

--- web/static/js/components/RecentGames.js
+++ web/static/js/components/RecentGames.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Table } from 'react-bootstrap'
 import champ_icons from '../css/StaticChampionSprites.css'
 import champions from '../data/champ_id_to_name.json'
+import TimeAgo from 'react-timeago'
 
 const RecentGames = props => {
     return(
@@ -20,9 +21,63 @@ const renderRecentGames = rows => (
 const renderRecentGame = row => (
     <tr className={row['stats']['win'] === true ? 'success' : 'danger'}>
         <td><i className={champ_icons["champion-" + row.championId]}></i>{' '}{champions[row.championId]}</td>
-        <td>{row['gameMode']} {row['gameType']} {row['subType']}</td>
+        <td>{gameLabel(row['subType'])}</td>
+        <td>{playerRoleLabel(row['stats']['playerRole'])} {playerPositionLabel(row['stats']['playerPosition'])}</td>
         <td>{row['stats']['championsKilled'] || '0'}/{row['stats']['numDeaths'] || '0'}/{row['stats']['assists'] || '0'}</td>
+        <td>{timeLabel(row['stats']['timePlayed'])}</td>
+        <td><TimeAgo date={row['createDate']} /></td>
     </tr>
 )
+
+function timeLabel(timePlayed) {
+    const seconds = timePlayed % 60
+    const secondsLabel = seconds > 0 ? `${seconds}s` : ''
+   return `${Math.round(timePlayed / 60)}m${secondsLabel}`
+}
+
+function gameLabel(subType) {
+    switch (subType) {
+        case 'RANKED_SOLO_5x5':
+            return 'Ranked'
+        case 'NORMAL':
+            return 'Normal'
+        case 'ARAM_UNRANKED_5x5':
+            return 'ARAM'
+        case 'BOT':
+            return 'Bots'
+        default:
+            return subType
+    }
+}
+
+function playerRoleLabel(playerRole) {
+    switch(playerRole) {
+        case 1:
+            return 'Duo'
+        case 2:
+            return 'Support'
+        case 3:
+            return 'Carry'
+        case 4:
+            return 'Solo'
+        default:
+            return playerRole
+    }
+}
+
+function playerPositionLabel(playerPosition) {
+    switch(playerPosition) {
+        case 1:
+            return 'Top'
+        case 2:
+            return 'Mid'
+        case 3:
+            return 'Jungle'
+        case 4:
+            return 'Bot'
+        default:
+            return playerPosition
+    }
+}
 
 export default RecentGames
